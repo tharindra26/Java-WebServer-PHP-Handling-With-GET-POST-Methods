f1586d256ea549f7658edcb68815b63e30d8fc0a
Tharindra <tharindragayashika@gmail.com>
2023-09-10 15:00:19 +0530

    Initial commit

---
+++ src/htdocs/index.php
@@ -0,0 +1,17 @@
+<html>
+<link rel="stylesheet" href="style.css">
+<body>
+<div class="container">
+  <div class="brand-title">Calculator</div>
+  <form action="calculate.php" method="post">
+  <div class="inputs">
+    <label>Number 1</label>
+    <input type="Number" placeholder="Enter num1" name="num1" />
+    <label>Number 2</label>
+    <input type="Number" placeholder="Enter num2" name="num2"/>
+    <button  type="submit" value="Submit">Add Numbers</button>
+  </div>
+  </form>
+</div>
+</body>
+</html>
---
+++ src/htdocs/other_docs/index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="calculate.php" method="post">
+    Enter first number: <input type="text" name="num1"><br>
+    Enter second number: <input type="text" name="num2"><br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
